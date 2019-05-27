ec50a87fd24ca508ec7f70e9092021e6a2681aee
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-27 17:13:21 -0300

    Remove translation from target attribute

--- react/legacy/FooterLinkList.js
+++ react/legacy/FooterLinkList.js
@@ -13,7 +13,7 @@ export const FooterLinkItem = ({ url, title, target, intl }) =>
         footer.listLink
       } dib no-underline underline-hover mb0 mt2 w-100 t-small truncate c-muted-1 pointer`}
       href={formatIOMessage({ id: url, intl })}
-      target={target ? formatIOMessage({ id: target, intl }) : '_self'}
+      target={target ? target : '_self'}
     >
       <IOMessage id={title} />
     </a>
