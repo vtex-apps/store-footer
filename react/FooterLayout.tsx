d469de00680e5c0b6ac509d230b7e1688bb60d4c
Lucas Bebber <lucasbbebber@gmail.com>
2019-07-01 10:16:59 -0300

    Add flex-grow element on top of footer

--- react/FooterLayout.tsx
+++ react/FooterLayout.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import style from './components/FooterLayout.css'
 
 const FooterLayout: React.FC = props => {
-  return <div className={style.footerLayout}>{props.children}</div>
+  return (
+    <>
+      <div className="flex flex-grow-1" />
+      <div className={style.footerLayout}>{props.children}</div>
+    </>
+  )
 }
 
 export default FooterLayout
