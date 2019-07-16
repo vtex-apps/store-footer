35749b507ae489baefdd95d572de599c349da5bb
Lucas Bebber <lucasbbebber@gmail.com>
2019-07-16 19:17:59 -0300

    Use vtex.device-detector for device detection

--- react/Footer.js
+++ react/Footer.js
@@ -1,45 +1,29 @@
 import React from 'react'
-import { ExtensionPoint, useChildBlock, useRuntime } from 'vtex.render-runtime'
+import { ExtensionPoint, useChildBlock } from 'vtex.render-runtime'
 import LegacyFooter from './legacy/Footer'
-import Media from 'react-media'
+import { useDevice } from 'vtex.device-detector'
 
 const Footer = props => {
-  const footerDesktop = !!useChildBlock({
+  const hasFooterDesktop = !!useChildBlock({
     id: 'footer-layout.desktop',
   })
-  const footerMobile = !!useChildBlock({
+  const hasFooterMobile = !!useChildBlock({
     id: 'footer-layout.mobile',
   })
 
-  const {
-    hints: { mobile },
-  } = useRuntime()
+  const { isMobile } = useDevice()
 
-  const hasFooterLayout = footerDesktop || footerMobile
+  const hasFooterLayout = hasFooterDesktop || hasFooterMobile
 
   if (!hasFooterLayout) {
     return <LegacyFooter {...props} />
   }
 
   // SSR fallback
-  if (!window || !window.matchMedia) {
-    return mobile ? (
-      <ExtensionPoint id="footer-layout.mobile" />
-    ) : (
-      <ExtensionPoint id="footer-layout.desktop" />
-    )
-  }
-
-  return (
-    <Media query="(max-width:40rem)">
-      {matches =>
-        matches ? (
-          <ExtensionPoint id="footer-layout.mobile" />
-        ) : (
-          <ExtensionPoint id="footer-layout.desktop" />
-        )
-      }
-    </Media>
+  return isMobile ? (
+    <ExtensionPoint id="footer-layout.mobile" />
+  ) : (
+    <ExtensionPoint id="footer-layout.desktop" />
   )
 }
 
