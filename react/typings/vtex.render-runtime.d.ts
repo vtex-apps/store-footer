99db6a7bf41474c90ce4966fb357332ea03a1cc0
victorhmp <victor2142@gmail.com>
2019-10-07 17:09:17 -0300

    Update render-runtime typings

--- react/typings/vtex.render-runtime.d.ts
+++ react/typings/vtex.render-runtime.d.ts
@@ -15,7 +15,66 @@ declare module 'vtex.render-runtime' {
 
   interface ExtensionPointProps {
     id: string
-    [key: string]: any
+    key: Record<string, any>
+  }
+
+  interface Pages {
+    [name: string]: Page
+  }
+
+  interface Route {
+    domain: string
+    blockId: string
+    canonicalPath?: string
+    id: string
+    params: Record<string, string>
+    path: string
+    title?: string
+  }
+
+  interface Culture {
+    availableLocales: string[]
+    locale: string
+    language: string
+    country: string
+    currency: string
+  }
+
+  export interface RenderRuntime {
+    account: string
+    accountId: string
+    appsEtag: string
+    workspace: string
+    disableSSR: boolean
+    hints: {
+      desktop: boolean
+      mobile: boolean
+      phone: boolean
+      tablet: boolean
+    }
+    page: string
+    route: Route
+    version: string
+    culture: Culture
+    pages: Pages
+    preview: boolean
+    production: boolean
+    publicEndpoint: string
+    renderMajor: number
+    query?: Record<string, string>
+    start: boolean
+    runtimeMeta: {
+      version: string
+      config?: any
+    }
+    settings: {
+      app: Record<string, any>
+    }
+    segmentToken: string
+    rootPath?: string
+    workspaceCookie: string
+    hasNewExtensions: boolean
+    platform: string
   }
 
   export const ExtensionPoint: ComponentType<ExtensionPointProps>
@@ -32,6 +91,7 @@ declare module 'vtex.render-runtime' {
   export const NoSSR: ReactElement
   export const RenderContextConsumer: ReactElement
   export const canUseDOM: boolean
+  export const useRuntime: () => RenderRuntime
   export const withRuntimeContext: <TOriginalProps extends {}>(
     Component: ComponentType<TOriginalProps & RenderContextProps>
   ) => ComponentType<TOriginalProps>
