c76c8da29d32269a7dc5d0e5b8b2aa98496c82fd
Lucas Bebber <lucasbbebber@gmail.com>
2020-02-12 15:13:18 -0300

    Differ fold types

--- react/hooks/useFold.tsx
+++ react/hooks/useFold.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react'
 import { useRuntime, canUseDOM } from 'vtex.render-runtime'
+import { useDevice } from 'vtex.device-detector'
 
+/** If there is a fold on the page, returns false until the user
+ * scrolls. This is a temporary hack, until partial hydration
+ * or something similar becomes available from render-runtime
+ */
 const useFold = () => {
   const { extensions, page } = useRuntime()
+  const { isMobile } = useDevice()
   const rootExtension = extensions[page]
   const { blocks } = rootExtension
 
   const hasFold = blocks?.some(({ extensionPointId }) =>
-    extensionPointId.includes('__fold__')
+    extensionPointId === '__fold__' ||
+    extensionPointId === `__fold__.${isMobile ? 'mobile' : 'desktop'}`
   ) ?? false
 
   const [shouldRender, setShouldRender] = useState(false)
