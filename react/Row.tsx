bd841744555451025768724abcccd1e1e916c8f8
Christian Kaisermann <christian@kaisermann.me>
2020-12-10 15:30:22 -0300

    Update and refactor Row

--- react/Row.tsx
+++ react/Row.tsx
@@ -1,22 +1,21 @@
+import type { PropsWithChildren } from 'react'
 import React from 'react'
 import classNames from 'classnames'
 import { useCssHandles } from 'vtex.css-handles'
 import { Container } from 'vtex.store-components'
 
-interface RowProps {
+interface Props {
+  /** Define if the row should take the full width of its container */
   fullWidth?: boolean
+  /** Define if background and text colors should be inverted */
   inverted?: boolean
   blockClass?: string
 }
 
 const CSS_HANDLES = ['rowContainer', 'row', 'rowContentContainer'] as const
 
-const Row: StorefrontFunctionComponent<RowProps> = ({
-  children,
-  fullWidth,
-  inverted,
-}) => {
-  const handles = useCssHandles(CSS_HANDLES)
+function Row({ children, fullWidth, inverted }: PropsWithChildren<Props>) {
+  const { handles } = useCssHandles(CSS_HANDLES)
 
   const content = (
     <div className={`${handles.rowContainer} w-100 flex items-center`}>
